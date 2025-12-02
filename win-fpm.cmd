98e3e93ec13ea74493832afe3a76a0eb55ebe634
andkrau <23638596+andkrau@users.noreply.github.com>
2025-12-02 16:11:07 -0600

    Allow for changing executable file name

--- win-fpm.cmd
+++ win-fpm.cmd
@@ -2,15 +2,16 @@
 call :READINI basePort
 call :READINI poolSize
 call :READINI phpDir
+call :READINI phpExe
 call :READINI fcgiChildren
 call :READINI listenHost
 call :READINI watchdogFrequency
 call :READINI phpOptions
 
 :START
-if not exist "%phpDir%/php-cgi.exe" goto NOPHP
+if not exist "%phpDir%/%phpExe%" goto NOPHP
 echo Killing all other running PHP processes
-wmic process where name="php-cgi.exe" delete>nul
+taskkill /F /IM "%phpExe%"
 echo Disabling PHP Max Requests Limit
 set PHP_FCGI_MAX_REQUESTS=0
 echo Setting Number of PHP FastCGI Child Processes
@@ -20,12 +21,12 @@ set /a totalChildren=%fcgiChildren% * %poolSize%
 if %fcgiChildren% GTR 0 set /a totalProcesses=%totalChildren% + %poolSize%
 set /a maxPort=%basePort% + %poolSize% - 1
 echo Creating pool of %poolSize% PHP parent processes with %totalChildren% total child processes
-for /l %%a in (%basePort%,1,%maxPort%) do echo Starting listener on port %%a && start /b "" "%phpDir%/php-cgi.exe" -b %listenHost%:%%a
+for /l %%a in (%basePort%,1,%maxPort%) do echo Starting listener on port %%a && start /b "" "%phpDir%/%phpExe%" -b %listenHost%:%%a
 echo %totalProcesses% total PHP processes started
 echo PHP FastCGI pool ready
 
 :WATCHDOG
-for /f "tokens=1,*" %%a in ('tasklist ^| find /I /C "php-cgi.exe"') do set running=%%a
+for /f "tokens=1,*" %%a in ('tasklist ^| find /I /C "%phpExe%"') do set running=%%a
 if %running% NEQ %totalProcesses% goto RESTART
 timeout /t %watchdogFrequency% /nobreak>nul
 goto WATCHDOG
@@ -44,6 +45,6 @@ pause
 exit
 
 :NOPHP
-echo php-cgi.exe not found in %phpDir%
+echo %phpExe% not found in %phpDir%
 pause
 exit
